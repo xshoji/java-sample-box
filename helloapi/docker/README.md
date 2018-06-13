7f2de23caea9169897455f43ccccd76086730ba9
george.n <biggy.keep@gmail.com>
2018-06-14 02:10:10 +0900

    パッケージ名を変更

--- helloapi/docker/README.md
+++
@@ -1,6 +0,0 @@
-
-特定のイメージに紐づくコンテナID一覧を出す
-
-```
-$ docker ps --filter="ancestor=mywebapp_java:0.1" -q
-```
--- helloapi/src/main/resources/db/migration/V1__Create_person_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(1000) NOT NULL,
-    display_name VARCHAR(1000),
-    created_on TIMESTAMP WITH TIME ZONE,
-    updated_on TIMESTAMP WITH TIME ZONE,
-    deleted_on TIMESTAMP WITH TIME ZONE
-);
