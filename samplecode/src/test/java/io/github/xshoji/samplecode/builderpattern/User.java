817034c1b13d896f4acf546b5dfdcb0aa8e10d56
xshoji <xshoji@users.noreply.github.com>
2019-01-23 02:06:01 +0900

    refactoring

--- samplecode/src/test/java/io/github/xshoji/samplecode/builderpattern/User.java
+++ samplecode/src/test/java/io/github/xshoji/samplecode/builderpattern/User.java
@@ -8,9 +8,7 @@ public class User {
   private Boolean isStudent;
   private Boolean isMarried;
   // 直接new禁止
-  private User(Builder b) {
-    name = b.name; nickname = b.nickname; states = b.states; city = b.city; isStudent = b.isStudent; isMarried = b.isMarried;
-  }
+  private User() {}
   public String getName()     { return name; }
   public String getNickname() { return nickname; }
   public String getStates()   { return states; }
@@ -18,23 +16,29 @@ public class User {
   public Boolean isStudent()  { return isStudent; }
   public Boolean isMarried()  { return isMarried; }
   public static class Builder {
-    private String name;
-    private String nickname;
-    private String states;
-    private String city;
-    private Boolean isStudent;
-    private Boolean isMarried;
+    private User provisionalUser;
     private Builder() {}
-    public static Builder builder() { return new Builder(); }
-    public Builder setName(String name)            { this.name = name;           return this; }
-    public Builder setNickname(String nickname)    { this.nickname = nickname;   return this; }
-    public Builder setStates(String states)        { this.states = states;       return this; }
-    public Builder setCity(String city)            { this.city = city;           return this; }
-    public Builder setIsStudent(Boolean isStudent) { this.isStudent = isStudent; return this; }
-    public Builder setIsMarried(Boolean isMarried) { this.isMarried = isMarried; return this; }
+    public static Builder builder() {
+      Builder builder = new Builder();
+      builder.provisionalUser = new User();
+      return builder;
+    }
+    public Builder setName(String name)            { provisionalUser.name = name;           return this; }
+    public Builder setNickname(String nickname)    { provisionalUser.nickname = nickname;   return this; }
+    public Builder setStates(String states)        { provisionalUser.states = states;       return this; }
+    public Builder setCity(String city)            { provisionalUser.city = city;           return this; }
+    public Builder setIsStudent(Boolean isStudent) { provisionalUser.isStudent = isStudent; return this; }
+    public Builder setIsMarried(Boolean isMarried) { provisionalUser.isMarried = isMarried; return this; }
     public User build() {
+      User user = new User();
+      user.name = provisionalUser.name;
+      user.nickname = provisionalUser.nickname;
+      user.states = provisionalUser.states;
+      user.city = provisionalUser.city;
+      user.isStudent = provisionalUser.isStudent;
+      user.isMarried = provisionalUser.isMarried;
       // 必要に応じてここで状態の整合性チェック
-      return new User(this);
+      return user;
     }
   }
 }
